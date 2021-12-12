dac2c4eecb83ecbb771b1cbfc05b8a0b73d27885
0512br <asa.pf1205@gmail.com>
2021-12-13 00:39:23 +0900

    define DOM element

---
+++ js-tab/tab.js
@@ -0,0 +1,9 @@
+// 即時関数
+(()=>{
+  const $doc = document;
+  const $tab = $doc.getElementById('js-tab');
+  // console.log($tab);
+  const $nav = $tab.querySelectorAll('[data-nav]');
+  const $content = $tab.querySelectorAll('[data-content]');
+  // console.log('$content', $content);
+})();

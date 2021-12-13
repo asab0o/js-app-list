5469b2ce77c64bbf7b43a9ffb37f416a11ef9ec5
0512br <asa.pf1205@gmail.com>
2021-12-13 13:30:22 +0900

    create init

--- js-tab/tab.js
+++ js-tab/tab.js
@@ -6,4 +6,10 @@
   const $nav = $tab.querySelectorAll('[data-nav]');
   const $content = $tab.querySelectorAll('[data-content]');
   // console.log('$content', $content);
+  // 初期化（文字列表示）
+  const init = () => {
+    $content[0].style.display = 'block';
+  };
+  init();
+
 })();
